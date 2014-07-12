e94bfe730745b05eff348593de97096c569d60ae
Daniel O'Connor <daniel.oconnor@gmail.com>
2014-07-12 22:14:18 +0930

    Naming

--- lib/ink3-rails/engine.rb
+++ lib/ink3-rails/engine.rb
@@ -1,7 +1,7 @@
-module Bootstrap
+module Ink
   module Rails
     class Engine < ::Rails::Engine
-      initializer 'ink.assets.precompile' do |app|
+      initializer 'ink3.assets.precompile' do |app|
         %w(css font imgs).each do |sub|
           app.config.assets.paths << root.join('vendor', 'assets', sub)
         end
